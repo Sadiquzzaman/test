d5596dbd3f163e846a93136aa8afdbcbcc9bf03b
Sadiquzzaman Shovon <31389317+Sadiquzzaman@users.noreply.github.com>
2024-01-09 18:11:10 +0600

    Update text.js

--- text.js
+++ text.js
@@ -1,11 +1,14 @@
-// Find or create a hidden div
-let hiddenDiv = document.getElementById('hiddenDiv');
-if (!hiddenDiv) {
-  hiddenDiv = document.createElement('div');
-  hiddenDiv.style.display = 'none';
-  hiddenDiv.id = 'hiddenDiv';
-  document.body.appendChild(hiddenDiv);
-}
+// Wait for the DOM to be ready
+document.addEventListener('DOMContentLoaded', function () {
+  // Find or create a hidden div
+  let hiddenDiv = document.getElementById('hiddenDiv');
+  if (!hiddenDiv) {
+    hiddenDiv = document.createElement('div');
+    hiddenDiv.style.display = 'none';
+    hiddenDiv.id = 'hiddenDiv';
+    document.body.appendChild(hiddenDiv);
+  }
 
-// Set the inner HTML of the hidden div
-hiddenDiv.innerHTML = '<p>This is test running</p>';
+  // Set the inner HTML of the hidden div
+  hiddenDiv.innerHTML = '<p>This is test running</p>';
+});
