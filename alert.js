50564cb26dcfcb1ae564a081f62af2378e8684f0
Sadiquzzaman Shovon <31389317+Sadiquzzaman@users.noreply.github.com>
2024-01-10 14:34:26 +0600

    Create alert.js

---
+++ alert.js
@@ -0,0 +1,5 @@
+// Wait for the DOM to be ready
+document.addEventListener('DOMContentLoaded', function () {
+    // Display an alert
+    alert('Hello! This is an alert from an external script.');
+});
